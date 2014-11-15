8f3f3881f673ff26c313dac16c4f97fe70660024
Dingyuan Wang <abcdoyle888@gmail.com>
2014-11-15 13:34:54 +0800

    Speed up the code and add 3D support

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 ### simplify.py
 
-simplify.py is a simple port of simplify.js by Vladimir Agafonkin ([https://github.com/mourner/simplify-js](https://github.com/mourner/simplify-js))
+simplify.py is a simple port of simplify.js by Vladimir Agafonkin
+([https://github.com/mourner/simplify-js](https://github.com/mourner/simplify-js))
+
+This fork speeds up the code. Because of the huge overhead in Python's dict, and
+few codes use dicts for points, I dropped the dict support.
 
 ### Usage
 
-```
+```python
 import simplify
-simplify(points, tolerance, highQuality)
+simplify.simplify(points, tolerance, highQuality)
 ```
 
-`points`: A sequence (e.g. tuple or array) of dictionaries, containing x, y coordinates: `{'x': int/float, 'y': int/float}`
-	or a sequence of sequences of at least two numbers (int or float), the first 
-	two elements of each inner sequence are treated as coordinates. Extra elements (e.g. z 
-	coordinate, name, etc) of both point sequences and dicts are ignored but preserved for 
-	points which remain. 
+`points`: A sequences of sequences of at least two numbers (int or float), the first
+	two elements of each inner sequence are treated as coordinates. Extra elements
+	(e.g. z coordinate, name, etc) of point sequences are ignored but preserved for
+	points which remain.
 
 Examples
-  * array of dict - `[{'x': 1, 'y': 1}, {'x': 2, 'y': 3}, {'x': 3, 'y': 5} ]`
   * tuple of tuple - `((1,1), (2,3), (3,5), (5,5))`
   * tuple of array - `([1,1,'first'], [2,3,'second'], [5,8,'third'])`
- 
+
 `tolerance (optional, 0.1 by default)`: Affects the amount of simplification that occurs (the smaller, the less simplification).
 
 `highestQuality (optional, True by default)`: Flag to exclude the distance pre-processing. Produces higher quality results, but runs slower.
+
+### 3D points
+
+Change the alias for 2D points: `simplify.changemode('2d')`
+Change the alias for 3D points: `simplify.changemode('3d')`
--- simplify.py
+++ simplify.py
@@ -1,7 +1,18 @@
+"""
+simplify.py is a simple port of simplify.js by Vladimir Agafonkin
+(https://github.com/mourner/simplify-js)
 
-def getSquareDistance_list(p1, p2):
+It uses a combination of Douglas-Peucker and Radial Distance algorithms.
+"""
+
+try:
+    rangefunc = xrange
+except NameError:
+    rangefunc = range
+
+def getSquareDistance2d(p1, p2):
     """
-    Square distance between two points
+    Square distance between two points (x,y)
     """
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
@@ -9,7 +20,18 @@ def getSquareDistance_list(p1, p2):
     return dx * dx + dy * dy
 
 
-def getSquareSegmentDistance_list(p, p1, p2):
+def getSquareDistance3d(p1, p2):
+    """
+    Square distance between two points (x,y,z)
+    """
+    dx = p1[0] - p2[0]
+    dy = p1[1] - p2[1]
+    dz = p1[2] - p2[2]
+
+    return dx * dx + dy * dy + dz * dz
+
+
+def getSquareSegmentDistance2d(p, p1, p2):
     """
     Square distance between point and a segment
     """
@@ -19,7 +41,7 @@ def getSquareSegmentDistance_list(p, p1, p2):
     dx = p2[0] - x
     dy = p2[1] - y
 
-    if dx != 0 or dy != 0:
+    if dx or dy:
         t = ((p[0] - x) * dx + (p[1] - y) * dy) / (dx * dx + dy * dy)
 
         if t > 1:
@@ -34,52 +56,60 @@ def getSquareSegmentDistance_list(p, p1, p2):
 
     return dx * dx + dy * dy
 
-def getSquareDistance_dict(p1, p2):
-    """
-    Square distance between two points
-    """
-    dx = p1['x'] - p2['x']
-    dy = p1['y'] - p2['y']
 
-    return dx * dx + dy * dy
-
-
-def getSquareSegmentDistance_dict(p, p1, p2):
+def getSquareSegmentDistance3d(p, p1, p2):
     """
     Square distance between point and a segment
     """
-    x = p1['x']
-    y = p1['y']
+    x = p1[0]
+    y = p1[1]
+    z = p1[2]
 
-    dx = p2['x'] - x
-    dy = p2['y'] - y
+    dx = p2[0] - x
+    dy = p2[1] - y
+    dz = p2[2] - z
 
-    if dx != 0 or dy != 0:
-        t = ((p['x'] - x) * dx + (p['y'] - y) * dy) / (dx * dx + dy * dy)
+    if dx or dy:
+        t = ((p[0] - x) * dx + (p[1] - y) * dy + (p[2] - z) * dz) / \
+            (dx * dx + dy * dy + dz * dz)
 
         if t > 1:
-            x = p2['x']
-            y = p2['y']
+            x = p2[0]
+            y = p2[1]
+            z = p2[2]
         elif t > 0:
             x += dx * t
             y += dy * t
+            z += dz * t
 
-    dx = p['x'] - x
-    dy = p['y'] - y
+    dx = p[0] - x
+    dy = p[1] - y
+    dz = p[2] - z
 
-    return dx * dx + dy * dy
+    return dx * dx + dy * dy + dz * dz
+
+
+def changemode(mode):
+    """
+    Change points to 2D/3D.
+    """
+    if mode == '2d':
+        getSquareDistance = getSquareDistance2d
+        getSquareSegmentDistance = getSquareSegmentDistance2d
+    elif mode == '3d':
+        getSquareDistance = getSquareDistance3d
+        getSquareSegmentDistance = getSquareSegmentDistance3d
+    else:
+        # use your own functions
+        pass
+
 
 def simplifyRadialDistance(points, tolerance):
-    point_is_dict = isinstance(points[0], dict)
 
-    getSquareDistance = getSquareDistance_dict if point_is_dict else getSquareDistance_list
-
-    length = len(points)
     prev_point = points[0]
     new_points = [prev_point]
 
-    for i in range(length):
-        point = points[i]
+    for point in points:
 
         if getSquareDistance(point, prev_point) > tolerance:
             new_points.append(point)
@@ -92,36 +122,28 @@ def simplifyRadialDistance(points, tolerance):
 
 
 def simplifyDouglasPeucker(points, tolerance):
-    point_is_dict = isinstance(points[0], dict)
-
-    getSquareSegmentDistance = getSquareSegmentDistance_dict if point_is_dict else getSquareSegmentDistance_list
-
-    length = len(points)
-    markers = [0] * length  # Maybe not the most efficent way?
 
     first = 0
-    last = length - 1
+    last = len(points) - 1
 
     first_stack = []
     last_stack = []
 
-    new_points = []
-
-    markers[first] = 1
-    markers[last] = 1
+    markers = [first, last]
 
     while last:
         max_sqdist = 0
 
-        for i in range(first, last):
-            sqdist = getSquareSegmentDistance(points[i], points[first], points[last])
+        for i in rangefunc(first, last):
+            sqdist = getSquareSegmentDistance(
+                points[i], points[first], points[last])
 
             if sqdist > max_sqdist:
                 index = i
                 max_sqdist = sqdist
 
         if max_sqdist > tolerance:
-            markers[index] = 1
+            markers.append(index)
 
             first_stack.append(first)
             last_stack.append(index)
@@ -129,26 +151,33 @@ def simplifyDouglasPeucker(points, tolerance):
             first_stack.append(index)
             last_stack.append(last)
 
-        # Can pop an empty array in Javascript, but not Python, so check
-        # the length of the list first
-        if len(first_stack) == 0:
-            first = None
-        else:
-            first = first_stack.pop()
+        # Can pop an empty array in Javascript, but not Python,
+        # so check the list first
+        first = first_stack.pop() if first_stack else None
+        last = last_stack.pop() if last_stack else None
 
-        if len(last_stack) == 0:
-            last = None
-        else:
-            last = last_stack.pop()
+    markers.sort()
 
-    for i in range(length):
-        if markers[i]:
-            new_points.append(points[i])
-
-    return new_points
+    return [points[i] for i in markers]
 
 
 def simplify(points, tolerance=0.1, highestQuality=True):
+    """
+    Simplifies the points.
+
+    `points`: A sequences of sequences of at least two numbers (int or float), the first
+        two elements of each inner sequence are treated as coordinates. Extra elements
+        (e.g. z coordinate, name, etc) of point sequences are ignored but preserved for
+        points which remain.
+
+    Examples
+      * tuple of tuple - `((1,1), (2,3), (3,5), (5,5))`
+      * tuple of array - `([1,1,'first'], [2,3,'second'], [5,8,'third'])`
+
+    `tolerance (optional, 0.1 by default)`: Affects the amount of simplification that occurs (the smaller, the less simplification).
+
+    `highestQuality (optional, True by default)`: Flag to exclude the distance pre-processing. Produces higher quality results, but runs slower.
+    """
     sqtolerance = tolerance * tolerance
 
     if not highestQuality:
@@ -157,3 +186,11 @@ def simplify(points, tolerance=0.1, highestQuality=True):
     points = simplifyDouglasPeucker(points, sqtolerance)
 
     return points
+
+getSquareDistance = getSquareDistance2d
+getSquareSegmentDistance = getSquareSegmentDistance2d
+
+if __name__ == '__main__':
+    import timeit
+    points = ((1, 1), (2, 3), (3, 5), (5, 5))
+    print(timeit.repeat(lambda: simplify(points)))
